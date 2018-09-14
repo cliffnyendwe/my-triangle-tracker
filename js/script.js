f53a36a65da4c7673706789371a7e64d484ed62a
cliffnyendwe"git config --global user.name "cliffnyendwe"git commit -m"add fix indentation error"git status <cliffnyendwe>
2018-09-14 14:59:37 +0300

    Add styles.css

--- js/script.js
+++ js/script.js
@@ -1,16 +1,35 @@
-var side1 = prompt('enter side 1:')
-var side2 = prompt('enter side 2:')
-var side3 = prompt('enter side 3:')
 
-if (side1==side2 && side2==side3){
-		window.alert('equilateral')
-}
-else if(side1==side2 && side2 !=side3){
-		window.alert('Isosceles')
-}
-else if(side1+side2<=side3){
-		window.alert('Not a triangle')
-}
-else {
-		window.alert('Scalene')
+
+function typeOfTriangle(){
+
+	var side1= document.querySelector('#triangle-side1').value;
+	console.log(side1);
+	var side2= document.querySelector('#triangle-side2').value;
+	console.log(side2);
+	var side3= document.querySelector('#triangle-side3').value;
+	console.log(side3);
+	var answer=document.querySelector('#answer');
+
+	// console.log('clickes')
+	if (side1==side2 && side2==side3){
+		console.log('equal')
+		// window.alert('equilateral')
+		answer.innerHTML = 'equilateral'
+	}
+	else if(side1==side2 && side2 !=side3){
+		console.log('iso')
+		// window.alert('Isosceles')
+		answer.innerHTML = 'Isosceles'
+	}
+	else if(side1+side2<=side3){
+		console.log('not')
+		// window.alert('Not a triangle')
+		answer.innerHTML = 'Not a triangle'
+	}
+	else {
+		console.log('scal	')
+		// window.alert('Scalene')
+		answer.innerHTML = 'Scalene'
+	}
+	answer.classList.remove('d-none')
 }
