e80d4065a84dba17aeb3a0a1830fe1df50dd1f9e
cliffnyendwe"git config --global user.name "cliffnyendwe"git commit -m"add fix indentation error"git status <cliffnyendwe>
2018-09-17 08:08:31 +0300

    add modify my pages

--- js/script.js
+++ js/script.js
@@ -13,13 +13,13 @@ function typeOfTriangle(){
 		 window.alert('equilateral')
 		answer.innerHTML = 'equilateral'
 	}
-	else if(side1==side2 && side2 !=side3){
+	else if(side1==side2 || side2 ==side3 || side1==side3){
 		console.log('iso')
 		 window.alert('Isosceles')
 		answer.innerHTML = 'Isosceles'
 	}
 
-	else if(side1+side2<=side3){
+	else if(side1+side2<=side3 || side2+side3>=side1 || side1+side3>=side2){
 		console.log('not')
 		window.alert('Not a triangle')
 		answer.innerHTML = 'Not a triangle'
